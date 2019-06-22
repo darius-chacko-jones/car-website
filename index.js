bf6a9f59c194cf065ad24152ef8fb32b8ce0f917
lojones <login.jones@gmail.com>
2019-06-22 18:13:45 -0400

    refactor method to create question htmls

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 
-    function showQuestion(submit,dropdown,response,correctAnswer) {
+
+    function createQuestion(parentDiv,questionNumber,questionText,answerOptionsArray,correctAnswer) {
+     
+        var qid = "q"+questionNumber;
+        $("#"+parentDiv).append("<div id='q"+questionNumber+"'></div>")
+        $("#"+qid).html("<div><strong>Q"+questionNumber+": </strong>"+questionText+"</div>");
+        var questionDropdownHtml = "<div><select id='"+qid+"-dropdown'><option value='0'>--Choose an answer--</option>";
+        for (var i=1;i<=answerOptionsArray.length;i++) {
+            var value=answerOptionsArray[i-1];
+            questionDropdownHtml += "<option value='"+i+"'>"+value+"</option>";
+        }
+        questionDropdownHtml += "</select></div>";
+        $("#"+qid).append(questionDropdownHtml);
+        $("#"+qid).append("<div><button id='"+qid+"-submit'>Submit</button></div>");
+        $("#"+qid).append("<div id='"+qid+"-response'></div>");
+
+        evaluateQuestion(qid+"-submit",qid+"-dropdown",qid+"-response",correctAnswer);
+
+
+  
+    }
+
+    function evaluateQuestion(submit,dropdown,response,correctAnswer) {
         $("#"+submit).click(function(){
             var index = $("#" + dropdown).val();
             var value = $("#" + dropdown).children("option:selected").html();
@@ -14,6 +36,9 @@ $(document).ready(function() {
         });
     }
 
-    showQuestion("q1-submit","q1-dropdown","q1-response",2);
+    
+
+    createQuestion("questions","1","Who is the inventor of electric cars?",["Elon Musk","Robert Anderson","Franz von Holzhausen","Darius Chacko Jones"],2);
+    
 });
 
